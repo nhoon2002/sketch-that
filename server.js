8113d6084cbb3780f95a4c19e8be526095e6be16
NH\nhoon_000 <nhoon2002@ucla.edu>
2017-02-01 23:17:56 -0800

    Login now routes to dashboard. Separated the firebase to another js. Having issues with mouse location calibration with the drawing board.

--- server.js
+++ server.js
@@ -1,59 +1,77 @@
-var firebase = require('firebase');
 var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var methodOverride = require('method-override');
-var sequelize = require('sequelize');
-var passport = require('passport');
-var exphbs = require("express-handlebars");
-var session = require("express-session");
-
-
-
-
-var port = process.env.PORT || 8080;
-
-// Requiring our models for syncing
-var db = require("./models");
-
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var path = require("path");
 
+app.use(express.static("./public"));
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(session({ secret: 'keyboard cat' }));
-app.use(passport.initialize());
-app.use(passport.session());
-// app.use(app.router);
+app.get("/", function(req, res) {
+ res.sendFile(path.join(__dirname + "/views/index.html"));
+});
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+app.get("/chatindex", function(req, res) {
+ res.sendFile(path.join(__dirname + "/views/chat-index.html"));
+});
 
-// // Set Handlebars.
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+app.get("/dashboard", function(req, res) {
+   res.sendFile(path.join(__dirname + "/views/dashboard.html"));
+});
 
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+users = [];
+connections = [];
 
-//Initialize Firebase
+// store draw history
+var line_history = [];
+// store chat history
+var chat_history = [];
 
+io.sockets.on('connection', function(socket) {
+  connections.push(socket);
+  console.log('Connected: %s sockets connected ', connections.length);
+  console.log('Users' +users);
 
+  // Disconnet
+  socket.on('disconnect', function(data) {
+     if(!socket.userName) return;
+    users.splice(users.indexOf(socket.userName), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected %s sockets connected ', connections.length);
+  });
 
+  // Send message
+  socket.on('send message', function(data) {
+    console.log('message from ' + socket.userName+ ': ' + data);
+    io.sockets.emit('new message', {msg: data, user: socket.userName});
+  });
 
+  // new user
+  socket.on('new user', function(data, callback) {
+    callback(true);
+    socket.userName = data;
+    users.push(socket.userName);
+    updateUsernames();
+  });
 
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  }
+  // -----------------------------------------------------------------------------
+  //--- Draw ---------------------------------------------------------------------
+   // first send the history to the new client
+   for (var i in line_history) {
+      socket.emit('draw_line', { line: line_history[i] } );
+   }
+   // add handler for message type "draw_line".
+   socket.on('draw_line', function (data) {
+      // add received line to history
+      line_history.push(data.line);
+      // send line to all clients
+      io.emit('draw_line', { line: data.line });
+   });
+});
 
-
-  app.listen(port, function() {
-    // // Cookies
-    // app.get('/', function(req, res) {
-    //   console.log('Cookies: ', req.cookies);
-    // });
-    console.log('Connected on port ' + port);
-
+server.listen(process.env.PORT || 3000, function(err, res) {
+  console.log('Listening on Port 3000');
 });
