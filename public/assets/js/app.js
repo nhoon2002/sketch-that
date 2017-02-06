63fad0001a9a41757f32447a520c6974dc958d17
Kevin Duong <duong.kev@gmail.com>
2017-02-06 10:34:32 -0800

    Updated for demo

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11,8 +11,11 @@ var database = firebase.database();
 var chatData = database.ref("/chat"); //chatbranch
 var playersRef = database.ref("players"); //playersbranch
 var currentTurnRef = database.ref("turn");
+var currentWordRef = database.ref("currentword");
 var currentUser_local = firebase.auth().currentUser;
-var username = "guest";
+var pointsp1 = 0;
+var pointsp2 = 0;
+var username = "guest"; //Can make this = currentUser_local.email.split("@")[0]; since firebase already knows if you are logged in or not.
 var currentPlayers = null;
 var currentTurn = null;
 var playerNum = false;
@@ -20,18 +23,84 @@ var playerOneExists = false;
 var playerTwoExists = false;
 var playerOneData = null;
 var playerTwoData = null;
+var currentWord;
+var wordBank = ['javascript', 'jquery', 'firebase', 'poop', 'node', 'extension', 'development', 'json', 'frontend', 'backend'];
+var points = 0;
+var guesser = null;
+// var clockrunning = false;
+var counter;
+// ------------------------------------------------
+// TIMER
+// Set our number counter to 100.
+
+        // Set our number counter to 100.
+        var number = 30;
+
+        // When the stop button gets clicked, run the stop function.
+        // $('#stop').on('click', stop);
+        // When the resume button gets clicked, execute the run function.
+        $('#modalOk').on('click', function() {
+          number = 30;
+            $("#drawing").html('<iframe src="https://fast-shore-34962.herokuapp.com/" id="canvasiframe" style="width:755px; height:400px;"></iframe>');
+        });
+
+        // The run function sets an interval
+        // that runs the decrement function once a second.
+        // Notice how we name the interval "counter."
+        function run(){
+            // clockrunning = true;
+            number = 30;
+            counter = setInterval(decrement, 1000);
+            console.log("running");
+        }
+
+        // The decrement function.
+        function decrement(){
+            // Decrease number by one.
+            number--;
+            // Show the number in the #show-number tag.
+            $('#timer').html('<h2>' + number + '</h2>');
+
+            // Once number hits zero...
+            if (number === 0){
+              $('#timer').html('<h2>Time Up!!</h2>');
+
+                // ...run the stop function.
+                stop();
+
+            }
+        }
+
+        // The stop function
+        function stop(){
+          // clockrunning = false;
+            // Clears our "counter" interval.
+            // We just pass the name of the interval
+            // to the clearInterval function.
+            clearInterval(counter);
+            console.log("stopping");
+
+        }
+
+        // Execute the run function.
+        // run();
+//END TIMER/////////////////////////////////////////////////////
 
 
 
 console.log("User: " + firebase.auth().currentUser.email);
 
 
-// // USERNAME LISTENERS
-// // Start button - takes username and tries to get user in game
-$("#buttonJoin").click(function() {
+// NOTE Sort of redundant. can get rid of the click trigger, bring out getInGame() to the outside to immediately start game.
+//  getInGame();
 
+if (playerTwoExists && playerOneExists) {
+  $("#buttonJoin").hide();
+}
+$("#buttonJoin").click(function() {
   username = currentUser_local.email.split("@")[0];
   console.log("current user: " + username);
+
   getInGame();
   $('#buttonJoin').hide();
 
@@ -45,11 +114,21 @@ $("#buttonJoin").click(function() {
 
 // CHAT LISTENERS
 // Chat send button listener, grabs input and pushes to firebase. (Firebase's push automatically creates a unique key)
+// if (number === 0) {
+//
+// }
 $("#chat-send").click(function() {
+
+
+
 
   if ($("#chat-input").val() !== "") {
 
     var message = $("#chat-input").val();
+    if (message === currentWord) {
+      guesser = playerNum;
+      turnWon(playerNum);
+    }
 
     chatData.push({
       name: username,
@@ -69,6 +148,10 @@ $("#chat-input").keypress(function(e) {
   if (e.keyCode === 13 && $("#chat-input").val() !== "") {
 
     var message = $("#chat-input").val();
+    if (message === currentWord) {
+      guesser = playerNum;
+      points++;
+    }
 
     chatData.push({
       name: username,
@@ -77,34 +160,18 @@ $("#chat-input").keypress(function(e) {
       idNum: playerNum
     });
 
+
     $("#chat-input").val("");
   }
+
 });
+
+
+
 
 // Click event for dynamically added <li> elements
-$(document).on("click", "li", function() {
-
-  console.log("click");
-
-  // Grabs text from li choice
-  var clickChoice = $(this).text();
-  console.log(playerRef);
-
-  // Sets the choice in the current player object in firebase
-  playerRef.child("choice").set(clickChoice);
-
-  // User has chosen, so removes choices and displays what they chose
-  $("#player" + playerNum + " ul").empty();
-  $("#player" + playerNum + "chosen").html(clickChoice);
-
-  // Increments turn. Turn goes from:
-  // 1 - player 1
-  // 2 - player 2
-  // 3 - determine winner
-  currentTurnRef.transaction(function(turn) {
-    return turn + 1;
-  });
-});
+// TODO: instead of "li", add an element on a modal which shows when 1) guesser correctly guesses the word or 2) timer is up.
+// TODO: on click of "next" (or moving on), increment turn.
 
 // Update chat on screen when new message detected - ordered by 'time' value
 chatData.orderByChild("time").on("child_added", function(snapshot) {
@@ -122,6 +189,43 @@ chatData.orderByChild("time").on("child_added", function(snapshot) {
 
   // Keeps div scrolled to bottom on each update.
   $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
+
+  if (snapshot.val().message === currentWord) {
+    console.log("guesser"+guesser);
+    console.log("points"+points);
+    console.log("player"+playerNum);
+    if (playerNum === guesser) {
+      $("#modalAnswer").text(snapshot.val().message);
+      $(".modal-body").text("You earned 1 Point! Nice work!");
+      currentTurnRef.transaction(function(turn) {
+        return turn + 1;
+      });
+
+    } else if (playerNum !== guesser) {
+      $(".modal-title").text("Correct answer was guessed!");
+      $(".modal-body").text("Prepare to guess next!");
+
+    }
+    stop();
+    $("#resultMsg").modal('toggle');
+    // guesser = null;
+
+  }
+
+
+  else if (number === 0) {
+    $("#modalAnswer").text(snapshot.val().message);
+    $(".modal-body").text("Time up. Nobody earned a point!");
+    currentTurnRef.transaction(function(turn) {
+      return turn + 1;
+    });
+    stop();
+    $("#resultMsg").modal('toggle');
+    // guesser = null;
+  }
+
+
+
 });
 
 // Tracks changes in key which contains player objects
@@ -169,9 +273,10 @@ playersRef.on("value", function(snapshot) {
 
 // Detects changes in current turn key
 currentTurnRef.on("value", function(snapshot) {
-
+  // stop();
   // Gets current turn from snapshot
   currentTurn = snapshot.val();
+
 
   // Don't do the following unless you're logged in
   if (playerNum) {
@@ -179,15 +284,19 @@ currentTurnRef.on("value", function(snapshot) {
     // For turn 1
     if (currentTurn === 1) {
 
+        run();
+        currentWordRef.set(wordBank[0]);
+        currentWord = wordBank[0];
       // If its the current player's turn, tell them and show choices
       if (currentTurn === playerNum) {
-        $("#result").html("<p>It's Your Turn!</p>");
-        $("#player" + playerNum + " ul").append("<li>Rock</li><li>Paper</li><li>Scissors</li>");
-      }
-      else {
+        $("#result").html("<p>It's Your Turn to Draw, "+playerOneData.name +"! The word is: </p>" + wordBank[0]);
 
+
+      }
+
+      else {
         // If it isnt the current players turn, tells them theyre waiting for player one
-        $("#result").html("<p>Waiting for " + playerOneData.name + " to choose.</p>");
+        $("#result").html("<p>It's Your Turn to Guess.<p>");
       }
 
       // Shows yellow border around active player
@@ -197,15 +306,19 @@ currentTurnRef.on("value", function(snapshot) {
 
     else if (currentTurn === 2) {
 
+          run();
+          //resets the current word to a random word
+          currentWordRef.set(wordBank[1]);
+          currentWord = wordBank[1];
       // If its the current player's turn, tell them and show choices
       if (currentTurn === playerNum) {
-        $("#result").html("<p>It's Your Turn!</p>");
-        $("#player" + playerNum + " ul").append("<li>Rock</li><li>Paper</li><li>Scissors</li>");
+        $("#result").html("<p>It's Your Turn to Draw, "+playerTwoData.name +"! The word is: </p>" + wordBank[1]);
+
       }
       else {
 
         // If it isnt the current players turn, tells them theyre waiting for player two
-        $("#current-turn").html("<h2>Waiting for " + playerTwoData.name + " to choose.</h2>");
+        $("#result").html("<p>It's your turn to guess.<p>");
 
       }
 
@@ -213,46 +326,93 @@ currentTurnRef.on("value", function(snapshot) {
       $("#player2").css("border", "2px solid yellow");
       $("#player1").css("border", "1px solid black");
     }
+    else if (currentTurn == 3) {
+      run();
+      //resets the current word to a random word
+      currentWordRef.set(wordBank[2]);
+      currentWord = wordBank[2];
+      // If its the current player's turn, tell them and show choices
+      if (currentTurn === (parseInt(playerNum)+2)) {
+        $("#result").html("<p>It's Your Turn to Draw, "+playerOneData.name +"! The word is: </p>" + wordBank[2]);
 
-    else if (currentTurn === 3) {
+      }
+      else {
 
+        // If it isnt the current players turn, tells them theyre waiting for player one
+        $("#result").html("<p>It's Your Turn to Guess.<p>");
+      }
+
+      // Shows yellow border around active player
+      $("#player1").css("border", "2px solid yellow");
+      $("#player2").css("border", "1px solid black");
+    }
+    else if (currentTurn === 4) {
+          run();
+          //resets the current word to a random word
+          currentWordRef.set(wordBank[3]);
+          currentWord = wordBank[3];
+
+      // If its the current player's turn, tell them and show choices
+      if (currentTurn == (parseInt(playerNum)+2)) {
+        $("#result").html("<p>It's Your Turn to Draw, "+playerTwoData.name +"! The word is: </p>" + wordBank[3]);
+      }
+      else {
+
+        // If it isnt the current players turn, tells them theyre waiting for player two
+        $("#result").html("<p>It's your turn to guess.<p>");
+
+      }
+      // Shows yellow border around active player
+      $("#player2").css("border", "2px solid yellow");
+      $("#player1").css("border", "1px solid black");
+    }
+
+///////////////RESULTS///////////////////////
+    else if (currentTurn === 5) {
+      $("#modalOk").on("click", function() {
+
+        gameLogic(points,username,playerNum);
+      });
       // Where the game win logic takes place then resets to turn 1
-      gameLogic(playerOneData.choice, playerTwoData.choice);
 
       // reveal both player choices
-      $("#player1-chosen").html(playerOneData.choice);
-      $("#player2-chosen").html(playerTwoData.choice);
+
+
 
       //  reset after timeout
       var moveOn = function() {
+        currentTurn = null;
+        points = 0;
+        guesser = null;
 
-        $("#player1-chosen").empty();
-        $("#player2-chosen").empty();
-        $("#result").empty();
+
 
         // check to make sure players didn't leave before timeout
         if (playerOneExists && playerTwoExists) {
           currentTurnRef.set(1);
+
+          console.log("Starting a new game!");
         }
       };
 
       //  show results for 2 seconds, then resets
-      setTimeout(moveOn, 2000);
+      $("#modalOk2").on("click", function() {
+          moveOn();
+      });
+
     }
 
-    else {
 
-      //  if (playerNum) {
-      //    $("#player" + playerNum + " ul").empty();
-      //  }
-      $("#player1 ul").empty();
-      $("#player2 ul").empty();
-      $("#result").html("<h6>Waiting for another player to join.</h6>");
-      $("#player2").css("border", "1px solid black");
-      $("#player1").css("border", "1px solid black");
-    }
+
   }
 });
+
+// Updates the current Word.
+currentWordRef.on("value", function(snapshot) {
+  currentWord = snapshot.val();
+  console.log(currentWord);
+});
+
 
 // When a player joins, checks to see if there are two players now. If yes, then it will start the game.
 playersRef.on("child_added", function(snapshot) {
@@ -272,6 +432,8 @@ function getInGame() {
   // so you can't ".push()" in a ".onDisconnect"
   var chatDataDisc = database.ref("/chat/" + Date.now());
 
+
+
   // Checks for current players, if theres a player one connected, then the user becomes player 2.
   // If there is no player one, then the user becomes player 1
   if (currentPlayers < 2) {
@@ -290,7 +452,8 @@ function getInGame() {
     playerRef.set({
       name: username,
       wins: 0,
-      losses: 0
+      losses: 0,
+      roundswon: 0,
       // choice: null
     });
 
@@ -310,7 +473,7 @@ function getInGame() {
     });
 
     // Remove name input box and show current player number.
-    // $("#notificationBar").html("<h2>Hi " + username + "! You are Player " + playerNum + "</h2>");
+
   }
   else {
 
@@ -321,54 +484,49 @@ function getInGame() {
 //
 // Game logic - Tried to space this out and make it more readable. Displays who won, lost, or tie game in result div.
 // Increments wins or losses accordingly.
-function gameLogic(player1choice, player2choice) {
-
-  var playerOneWon = function() {
-    $("#result").html("<h2>" + playerOneData.name + "</h2><h2>Wins!</h2>");
-    if (playerNum === 1) {
-      playersRef.child("1").child("wins").set(playerOneData.wins + 1);
-      playersRef.child("2").child("losses").set(playerTwoData.losses + 1);
-    }
-  };
-
-  var playerTwoWon = function() {
-    $("#result").html("<h2>" + playerTwoData.name + "</h2><h2>Wins!</h2>");
-    if (playerNum === 2) {
-      playersRef.child("2").child("wins").set(playerTwoData.wins + 1);
-      playersRef.child("1").child("losses").set(playerOneData.losses + 1);
-    }
-  };
-
-  var tie = function() {
-    $("#result").html("<h2>Tie Game!</h2>");
-  };
-
-  if (player1choice === "Rock" && player2choice === "Rock") {
-    tie();
-  }
-  else if (player1choice === "Paper" && player2choice === "Paper") {
-    tie();
-  }
-  else if (player1choice === "Scissors" && player2choice === "Scissors") {
-    tie();
-  }
-  else if (player1choice === "Rock" && player2choice === "Paper") {
-    playerTwoWon();
-  }
-  else if (player1choice === "Rock" && player2choice === "Scissors") {
-    playerOneWon();
-  }
-  else if (player1choice === "Paper" && player2choice === "Rock") {
-    playerOneWon();
-  }
-  else if (player1choice === "Paper" && player2choice === "Scissors") {
-    playerTwoWon();
-  }
-  else if (player1choice === "Scissors" && player2choice === "Rock") {
-    playerTwoWon();
-  }
-  else if (player1choice === "Scissors" && player2choice === "Paper") {
-    playerOneWon();
-  }
+function turnWon(playerNum) {
+    playersRef.child(playerNum).child("roundswon").transaction(function(roundswon) {
+      return roundswon + 1;
+    });
+    console.log("Player"+playerNum+" won a point.");
 }
-}, 2000);
+playersRef.child("1").child("roundswon").on("value", function(snapshot) {
+  pointsp1 = snapshot.val();
+  console.log("pointsp1 = "+pointsp1);
+
+});
+playersRef.child("2").child("roundswon").on("value", function(snapshot) {
+  pointsp2 = snapshot.val();
+  console.log("pointsp2 = "+pointsp2);
+});
+function gameLogic(pointsp1,pointsp2, username, playerNum) {
+  if (pointsp1 > pointsp2) {
+    $(".modal-title").text("Player 1 won!");
+    $(".modal-body").text("");
+    playersRef.child("1").child("wins").transaction(function(wins) {
+      return wins + 1;
+    });
+    playersRef.child("2").child("losses").transaction(function(losses){
+      return losses + 1;
+    });
+
+  } else if (pointsp1 < pointsp2) {
+    $(".modal-title").text("Player 2 won!");
+    $(".modal-body").text("");
+    playersRef.child("2").child("wins").transaction(function(wins){
+      return wins + 1;
+    });
+    playersRef.child("1").child("losses").transaction(function(losses){
+      return losses + 1;
+    });
+  } else if (pointsp1 === pointsp2) {
+    $(".modal-title").text("It's a draw!");
+    $(".modal-body").text("");
+  }
+
+  $("#endMsg").modal('toggle');
+}
+
+
+
+}, 3000);
